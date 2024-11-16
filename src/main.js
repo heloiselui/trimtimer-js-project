9834797dceeb33bfbfbaf71f597ce7eda3d6cb2d
Heloise Lui <helolui27@gmail.com>
2024-11-16 13:30:31 +0000

    Add Day.js to the application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+// Use Strict makes JavaScript code safer and easier to debug. 
 "use strict"
+
+// Setting up the dayjs
+import "./libs/dayjs.js"
 
 // CSS
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
+
